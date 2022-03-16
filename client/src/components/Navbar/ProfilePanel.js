260415e78c9e064b763ed76c603377ef9a964e76
Lim Zheng Tao <zhengtaolim@gmail.com>
2022-03-17 02:28:43 +0800

    Signup Dropdown and Logout to respective pages

--- client/src/components/Navbar/ProfilePanel.js
+++ client/src/components/Navbar/ProfilePanel.js
@@ -12,9 +12,11 @@ const ProfilePanel = () => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
     const token = user?.token; // check if token exists
     const [open, setOpen] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
     const history = useHistory();
     const dispatch = useDispatch();
     const anchorRef = React.useRef(null);
+    const anchorRef2 = React.useRef(null);
     const location = useLocation(); // when location change, set the user
 
     // JSON web token (Manual login)
@@ -31,11 +33,19 @@ const ProfilePanel = () => {
 
     const logout = (event) => {
         dispatch({ type: actionType.LOGOUT }); // logout
-        history.push("/displaylistings"); // redirect back to displaylistings
+        if (user?.result.type === "donor" ){
+            history.push("/donor");
+        }
+        else if(user?.result.type === "donatee" ){
+            history.push("/donatee");
+        }
+        else if(user?.result.type === "admin" ){
+            history.push("/donor");
+        }
         setUser(null);
         handleClose(event);
     };
-
+    // For right button
     const handleToggle = () => {
         setOpen((prevOpen) => !prevOpen);
     };
@@ -56,6 +66,27 @@ const ProfilePanel = () => {
         }
     }
 
+    // For left button
+    const handleToggle2 = () => {
+        setOpen2((prevOpen) => !prevOpen);
+    };
+    
+    const handleClose2 = (event) => {
+        if (anchorRef2.current && anchorRef2.current.contains(event.target)) {
+            return;
+        }   
+        setOpen2(false);
+    };
+    
+    function handleListKeyDown2(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen2(false);
+        } else if (event.key === 'Escape') {
+            setOpen2(false);
+        }
+    }
+
     if (user?.result.type === "donor" ){
         return(   
             <DonorProfile user={user}  logout={logout} anchorRef={anchorRef} open={open} handleToggle={handleToggle} handleClose={handleClose} handleListKeyDown={handleListKeyDown}/>
@@ -73,7 +104,7 @@ const ProfilePanel = () => {
     }
     // if not login
     return(   
-        <UserProfile anchorRef={anchorRef} open={open} handleToggle={handleToggle} handleClose={handleClose} handleListKeyDown={handleListKeyDown}/>
+        <UserProfile anchorRef={anchorRef} open={open} handleToggle={handleToggle} handleClose={handleClose} handleListKeyDown={handleListKeyDown} anchorRef2={anchorRef2} open2={open2} handleToggle2={handleToggle2} handleClose2={handleClose2} handleListKeyDown2={handleListKeyDown2}/>
     );
 }
 
