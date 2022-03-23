d9cfded04d51e3ad2f350122b0d394a8e9405889
Lim Zheng Tao <zhengtaolim@gmail.com>
2022-03-23 18:20:19 +0800

    [Admin IndividualDonation} Change Ui

--- client/src/components/DonationListing/IndividualDonationDetails.js
+++ client/src/components/DonationListing/IndividualDonationDetails.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Box,Card,Stack,Typography,Container,Grid,Button } from '@mui/material'
+import { Box,Stack,Typography,Container,Button } from '@mui/material'
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card'
 import { useLocation,Link, useHistory} from 'react-router-dom'
 import {useSelector,useDispatch} from 'react-redux'
 import {getDonation,updateDonation} from '../../actions/donationListing.js'
@@ -47,70 +49,84 @@ const IndividualDonationDetails = () => {
     }
 
     return (
-   
-        <Grid sx={{ml:"15%"}} container spacing={20}>
-            <Grid item xs={4}>
-                <Card sx={{ maxWidth: 500 }}>
-                    <CardMedia
-                        component="img"
-                        height="750"
-                        image="https://picsum.photos/id/239/1000/1000"
-                    />
-                </Card>
-            </Grid>
-            <Grid item xs={8}>
-                <Stack spacing={4} sx={{maxWidth:"30%"}}>
-                        { isAdmin &&
-                        <Box sx={{ display: 'flex', flexDirection: 'row-reverse', mb:2}}>
-                            <CancelSharpIcon color='error' fontSize="large" onClick={handleClose} />
-                        </Box>}
-                   {isAdmin && donationDetails[0]?.status == 'Pending' &&
-                   <Box>
-                        <Box sx={{ display: 'flex', flexDirection: 'row-reverse',gap:3 }}>
-                            <Button variant='contained' color='error' onClick={()=>changeRequest('Rejected')}>Reject</Button>
-                            <Button variant='contained' color='success' onClick={()=>changeRequest('Approved')}>Accept</Button>
+        <Container disableGutters = "true" maxWidth = "xl" sx={{paddingLeft:"8px", paddingRight:"10px"}}>
+            <Grid container spacing={5}>
+                <Grid item xs={5}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                           
+                            image="https://picsum.photos/id/239/1000/1000"
+                        />
+                    </Card>
+                </Grid>
+                <Grid item xs={7}>
+                    <Stack sx={{backgroundColor:"white", borderRadius:"5px",  padding:2}}>
+                        <Grid container sx={{mb: 1}}>
+                            
+                            
+                            <Grid item xs={11}>
+                                <Typography variant="h3">{donationDetails[0]?.name}</Typography>
+                            </Grid>
+                            { isAdmin &&
+                                <Grid item xs={1}>
+                                    <Box sx={{ display: 'flex', flexDirection: 'row-reverse', }}>
+                                        <CancelSharpIcon color='error' fontSize="large" onClick={handleClose} />
+                                    </Box>
+                                </Grid>
+                            }
+                        </Grid>
+                        {isAdmin && donationDetails[0]?.status == 'Pending' &&
+                                
+                            <Box sx={{ display: 'flex', flexDirection: 'row',gap:3, my:1 }}>
+                                <Button variant='contained' color='error' onClick={()=>changeRequest('Rejected')}>Reject</Button>
+                                <Button variant='contained' color='success' onClick={()=>changeRequest('Approved')}>Accept</Button>
+                            </Box>
+                                
+                        }
+                        <Grid sx={{my: 1}}>
+                            <Typography  variant='h5'>{date?.getDate()}/{date?.getMonth()}/{date?.getFullYear()}</Typography>
+                        </Grid>
+                        <Grid sx={{my: 1}}>
+                            <Typography sx={{wordWrap:"break-word"}} variant='h5' align="justify">{donationDetails[0]?.description == undefined? placeholderDescription: donationDetails[0]?.description}</Typography>
+                        </Grid>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <Box sx={{ width: '100%', mr: 1 }}>
+                                <LinearProgress variant="determinate" color="secondary" sx={{ height: 10, borderRadius: 1 }} value={progress} />
+                            </Box>
+                            <Box>
+                                <Typography>{progress}%</Typography>
+                            </Box>
                         </Box>
+                        <Typography sx={{mt:10}} variant="h4">${donationDetails[0]?.totalAmountCollected} collected of ${donationDetails[0]?.donationValue}</Typography>
                         
-                    </Box>}
-                    <Typography variant="h3">{donationDetails[0]?.name}</Typography>
-                    <Typography variant='h5'>{date?.getDate()}/{date?.getMonth()}/{date?.getFullYear()}</Typography>
-                    <Typography sx={{wordWrap:"break-word"}} variant='h5' align="justify">{donationDetails[0]?.description == undefined? placeholderDescription: donationDetails[0]?.description}</Typography>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Box sx={{ width: '100%', mr: 1 }}>
-                            <LinearProgress variant="determinate" color="secondary" sx={{ height: 10, borderRadius: 1 }} value={progress} />
-                        </Box>
-                        <Box>
-                            <Typography>{progress}%</Typography>
-                        </Box>
-                    </Box>
-                    <Typography sx={{mt:10}} variant="h4">${donationDetails[0]?.totalAmountCollected} collected of ${donationDetails[0]?.donationValue}</Typography>
-                    
-                    {isDonor && <Grid columns={2} container spacing={0}  rowSpacing={2} >
-                    
-                       <Grid item xs={1}>
-                            <TermsnConditions custom={false} id={id} buttonValue="$5" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
-                        </Grid>
-
+                        {isDonor && <Grid columns={2} container spacing={0}  rowSpacing={2} >
+                        
                         <Grid item xs={1}>
-                            <TermsnConditions custom={false} id={id} buttonValue="$10" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
-                        </Grid>
+                                <TermsnConditions custom={false} id={id} buttonValue="$5" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
+                            </Grid>
 
-                        <Grid item xs={1}>
-                            <TermsnConditions custom={false} id={id} buttonValue="$50" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
-                        </Grid>
+                            <Grid item xs={1}>
+                                <TermsnConditions custom={false} id={id} buttonValue="$10" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
+                            </Grid>
 
-                        <Grid item xs={1}>
-                            <TermsnConditions custom={false} id={id} buttonValue="$1000" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
-                        </Grid>
+                            <Grid item xs={1}>
+                                <TermsnConditions custom={false} id={id} buttonValue="$50" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
+                            </Grid>
 
-                        <Grid item xs={2}>
-                            <TermsnConditions custom id={id} buttonValue="Custom Amount" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
-                        </Grid>
+                            <Grid item xs={1}>
+                                <TermsnConditions custom={false} id={id} buttonValue="$1000" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
+                            </Grid>
 
-                    </Grid >}
-                </Stack>
+                            <Grid item xs={2}>
+                                <TermsnConditions custom id={id} buttonValue="Custom Amount" donationValue = {[donationDetails[0]?.totalAmountCollected,donationDetails[0]?.donationValue]}/>
+                            </Grid>
+
+                        </Grid >}
+                    </Stack>
+                </Grid>
             </Grid>
-        </Grid>
+        </Container>
   )
 }
 
